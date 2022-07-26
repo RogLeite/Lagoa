c73652207d00a6d8c44671860afaabb4f6404bbb
RogLeite <rodrigo.ldasilva@hotmail.com>
2022-07-26 12:16:41 -0300

    v0.3.0.4 ExceptionHandler Delegate
    v0.3.0.4 ExceptionHandler Delegate for ServerConnection
    
    # ExceptionHandler.gd:
     - Handler Nakama's Async results
     - Stores the error messages
    
    # ServerConnection.gd:
     - Uses ExceptionHandler to parse Async results
     - Stores error messages en member `error_message`

--- pond-project/src/Autoload/ServerConnection.gd
+++ pond-project/src/Autoload/ServerConnection.gd
@@ -15,6 +15,9 @@ enum OpCodes {
 # Unique key for the server, defined in it's "docker-compose.yml" file
 const KEY := "nakama_pond_server"
 
+# String that contains the error message whenever any of the functions that yield return != OK
+var error_message := "" setget _no_set, _get_error_message
+
 # Stores the authentication token for the user's session in the server
 var _session: NakamaSession
 
@@ -27,35 +30,33 @@ var _socket : NakamaSocket
 # The identifier of the match this client participates
 var _world_id:= ""
 
+var _exception_handler := ExceptionHandler.new()
+
 # Starts the Nakama Client and connects it to the server
 func start_client():
 # [TODO] When the server is hosted non-locally, change the IP address used
 	 _client = Nakama.create_client(KEY, "127.0.0.1", 7350, "http")
 
 # Authenticates a new session from the given email and password. If it's a new user, registers the credentials
-# Can return: 
-# 	OK if successful
-# 	ERR_CANT_CONNECT if authenticate_email_async() failed
+# Returns OK or a nakama error code. Stores error messages in `ServerConnection.error_message`
 # [TODO] Split Registering from Authenticating
 func authenticate_async(email : String, password : String) -> int :
 	assert(_client, "_client was not initialized, remember to call ServerConnection.start_client()")		 
-	
-	var result := OK
 	
 	var should_create := true
 
 	var new_session: NakamaSession = \
 		yield(_client.authenticate_email_async(email, password, email, should_create), "completed")	
 	
-	if not new_session.is_exception():
+	var result := _exception_handler.parse_exception(new_session)
+
+	if result == OK:
 		_session = new_session
-	else:
-		result = new_session.get_exception().status_code
 		
 	return result
 
 # Starts the socket connection with the server, if possible
-# returns ERR_CANT_CONNECT if socket could not connect to the session
+# Returns OK or a nakama error code. Stores error messages in `ServerConnection.error_message`
 func connect_to_server_async() -> int :
 	assert(_client, "_client was not initialized, remember to call ServerConnection.start_client()")
 	assert(_session, "_session was not initialized, remember to call ServerConnection.authenticate_async()")
@@ -66,45 +67,59 @@ func connect_to_server_async() -> int :
 	var result: NakamaAsyncResult = \
 		yield(_socket.connect_async(_session), "completed")
 
-	# Connection failed, returns error
-	if result.is_exception():
-		return ERR_CANT_CONNECT
+	var parsed_result := _exception_handler.parse_exception(result)
 
-	# Connection was opened, connect to signals in the _socket
-	_socket.connect("closed", self, "_on_NakamaSocket_closed")
-	_socket.connect("received_match_state", self, "_on_NakamaSocket_received_match_state")
-	return OK
+	if parsed_result == OK:
+		# Connection was opened, connect to signals in the _socket
+		_socket.connect("closed", self, "_on_NakamaSocket_closed")
+		_socket.connect("received_match_state", self, "_on_NakamaSocket_received_match_state")
+
+	return parsed_result
 
 # Remote calls `get_world_id()` then joins the match
-func join_world_async(): 
+# Returns OK or a nakama error code. Stores error messages in `ServerConnection.error_message`
+func join_world_async() -> int: 
 	# Debug assertions
 	assert(_client, "_client was not initialized, remember to call ServerConnection.start_client()")
 	assert(_socket, "_socket was not initialized, remember to call ServerConnection.connect_to_server_async()")
 	
 	# Non-debug assertions
 	if not _socket:
-		printerr("Server not connected.")
-		return
+		_exception_handler.error_message = "Server not connected"
+		return ERR_UNAVAILABLE
 	
 	# Gets the world id from the server with RPC to `get_world_id()`
-	var rpc_result : NakamaAPI.ApiRpc = yield(_client.rpc_async(_session,"get_world_id", ""), "completed")
+	if not _world_id:
+		var rpc_result : NakamaAPI.ApiRpc = yield(_client.rpc_async(_session,"get_world_id", ""), "completed")
 
-	if not rpc_result.is_exception():
+		var parsed_result := _exception_handler.parse_exception(rpc_result)
+
+		if parsed_result != OK:
+			return parsed_result
+
 		_world_id = rpc_result.payload
-	else: 
-		var exception: NakamaException = rpc_result.get_exception()
-		printerr("RPC returned exception: %s - %s." % [exception.status_code, exception.message])
-		return
+		
 
 	# Joins the match represented by _world_id
 	# [TODO] Maybe the metadata sent can identify if this client wants to be a MasterClient
 	var match_join_result: NakamaRTAPI.Match = \
 		yield(_socket.join_match_async(_world_id), "completed")
-	# If join failed, prints error message
-	if match_join_result.is_exception():
-		var exception : NakamaException = match_join_result.get_exception()
-		printerr("Error joining the match: %s - %s" % [exception.status_code, exception.message])
-		return
+	var parsed_result := _exception_handler.parse_exception(match_join_result)
+
+	if parsed_result == OK:
+		# Can enter chat, register presences from match_join_result etc.
+		pass
+	else:
+		return parsed_result
+
+	return parsed_result
+
+# [TODO] Fazer este métoto
+func disconnect_from_server_async() -> int:
+	return OK
+
+func _get_error_message() -> String:
+	return _exception_handler.error_message
 
 # De-references the _socket object
 func _on_NakamaSocket_closed() -> void:
@@ -123,11 +138,15 @@ func _on_NakamaSocket_received_match_state(match_state : NakamaRTAPI.MatchData) 
 			var current_tick: int = decoded.current_tick
 			print("Current server current_tick: %d"%current_tick)
 
+# Used as a setter function for read-only variables.
+func _no_set(_value) -> void:
+	pass
+
 # [TODO] Remove this method. It is only used for debugging.
-# func _ready():
-# 	start_client()
-# 	var email := "test1@pond.com"
-# 	var password := "password"
-# 	print("Authenticate: %d"%yield( authenticate_async(email, password), "completed" ))
-# 	print("Connect to Server: %d"%yield( connect_to_server_async(), "completed" ))
-# 	yield( join_world_async(), "completed" )
+func _ready():
+	start_client()
+	var email := "test1@pond.com"
+	var password := "password"
+	print("Authenticate: %d"%yield( authenticate_async(email, password), "completed" ))
+	print("Connect to Server: %d"%yield( connect_to_server_async(), "completed" ))
+	yield( join_world_async(), "completed" )
