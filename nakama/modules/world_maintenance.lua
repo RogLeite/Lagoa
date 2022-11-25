019edbc9e713cdd5242b3caf93ba0672097c2906
RogLeite <rodrigo.ldasilva@hotmail.com>
2022-11-25 12:14:16 -0300

    v0.3.0.67 Puts world maintenance in module
    v0.3.0.67 Puts world maintenance in lua module
    
    # world_maintenance.lua:
     - Provides functionality for world maintenance in the server
    
    # world_rpc.lua:
     - Moves implementation of world maintenance to new module

---
+++ nakama/modules/world_maintenance.lua
@@ -0,0 +1,57 @@
+local M = {}
+
+function M.new_world(worlds, world_id)
+    worlds[world_id] = {
+        master = false,                 -- user_id of master client
+        player_reservations = {},       -- Array of players user_id in the match
+        -- player_reservations_count = 0,  -- Counter for player reservations. If is equal to MAX_PLAYERS_PER_MATCH, only lets players in `player_reservations` join
+        player_presences = {}           -- Table of user_id:presence
+     }
+end
+function M.get_world(worlds, world_id)
+    return worlds[world_id]
+end
+function M.is_reserved(worlds, world_id, user_id)
+    for k,v in ipairs(M.get_world(worlds, world_id).player_reservations) do
+        if v == user_id then
+            return k
+        end
+    end
+    return false
+end
+function M.is_present(worlds, world_id, user_id)
+    return M.get_world(worlds, world_id).player_presences[user_id] and true
+end
+function M.add_master(worlds, world_id, user_id)
+    M.get_world(worlds, world_id).master = user_id
+end
+function M.remove_master(worlds, world_id)
+    M.get_world(worlds, world_id).master = false
+end
+function M.add_reservation(worlds, world_id, user_id)
+    if not M.is_reserved(worlds, world_id, user_id) then
+        local res = M.get_world(worlds, world_id).player_reservations
+        res[#res+1] = user_id
+    end
+end
+-- function M.remove_reservation(world_id, user_id)
+--     local reservation = is_reserved(world_id, user_id)
+-- end
+function M.add_presence(worlds, world_id, user_presence)
+    local id = user_presence.user_id
+    -- nakama.logger_warn(string.format("add_presence: user_presence.user_id = '%s', keys(user_presence) = {%s}", user_presence.user_id, table_keys(user_presence)))
+    if not M.is_present(worlds, world_id, id) then
+        local world = M.get_world(worlds, world_id)
+        local presences = world.player_presences
+        -- nakama.logger_warn(string.format("add_presence: type(id) is \"%s\"", type(id)))
+        presences[id] = user_presence
+    end
+end
+function M.remove_presence(worlds, world_id, user_presence)
+    local id = user_presence.user_id
+    if M.is_present(worlds, world_id, id) then
+        M.get_world(worlds, world_id).player_presences[id] = nil
+    end
+end
+
+return M
--- nakama/modules/world_rpc.lua
+++ nakama/modules/world_rpc.lua
@@ -1,4 +1,5 @@
 local nakama = require("nakama")
+local worlds = require("world_maintenance")
 
 local MAX_PLAYERS_PER_MATCH = 4
 
@@ -11,61 +12,7 @@ local function table_keys(t)
     return table.concat(ret, ", ")
 end
 
-local worlds = {}
-
-local function new_world(world_id)
-    worlds[world_id] = {
-        master = false,                 -- user_id of master client
-        player_reservations = {},       -- Array of players user_id in the match
-        -- player_reservations_count = 0,  -- Counter for player reservations. If is equal to MAX_PLAYERS_PER_MATCH, only lets players in `player_reservations` join
-        player_presences = {}           -- Table of user_id:presence
-     }
-end
-local function get_world(world_id)
-    return worlds[world_id]
-end
-local function is_reserved(world_id, user_id)
-    for k,v in ipairs(get_world(world_id).player_reservations) do
-        if v == user_id then
-            return k
-        end
-    end
-    return false
-end
-local function is_present(world_id, user_id)
-    return get_world(world_id).player_presences[user_id] and true
-end
-local function add_master(world_id, user_id)
-    get_world(world_id).master = user_id
-end
-local function remove_master(world_id)
-    get_world(world_id).master = false
-end
-local function add_reservation(world_id, user_id)
-    if not is_reserved(world_id, user_id) then
-        local res = get_world(world_id).player_reservations
-        res[#res+1] = user_id
-    end
-end
--- local function remove_reservation(world_id, user_id)
---     local reservation = is_reserved(world_id, user_id)
--- end
-local function add_presence(world_id, user_presence)
-    local id = user_presence.user_id
-    -- nakama.logger_warn(string.format("add_presence: user_presence.user_id = '%s', keys(user_presence) = {%s}", user_presence.user_id, table_keys(user_presence)))
-    if not is_present(world_id, id) then
-        local world = get_world(world_id)
-        local presences = world.player_presences
-        -- nakama.logger_warn(string.format("add_presence: type(id) is \"%s\"", type(id)))
-        presences[id] = user_presence
-    end
-end
-local function remove_presence(world_id, user_presence)
-    local id = user_presence.user_id
-    if is_present(world_id, id) then
-        get_world(world_id).player_presences[id] = nil
-    end
-end
+local world_table = {}
 
 local function _get_first_world()
     local matches = nakama.match_list()
@@ -79,7 +26,7 @@ local function _get_first_world()
         world_id = nakama.match_create("world_control", {})
 
         -- Creates a new information table for the created match
-        new_world(world_id)
+        worlds.new_world(world_table, world_id)
     else 
         world_id = current_match.match_id
     end
@@ -103,10 +50,10 @@ local function match_join_maintenance(world_id, presence, metadata)
     local user_id = presence.user_id
 
     if metadata and string.lower(metadata.is_master) == "true" then
-        add_master(world_id, user_id)
+        worlds.add_master(world_table, world_id, user_id)
     else
-        add_reservation(world_id, user_id)
-        add_presence(world_id, presence)
+        worlds.add_reservation(world_table, world_id, user_id)
+        worlds.add_presence(world_table, world_id, presence)
     end
 end
 
@@ -114,11 +61,11 @@ local function match_leave_maintenance(world_id, presence)
     -- nakama.logger_warn(string.format("match_leave_maintenance: presence.user_id = '%s'", presence.user_id))
     -- nakama.logger_warn(string.format("match_leave_maintenance: presence.user_id = '%s', keys(presence) = {%s}", presence.user_id, table_keys(presence)))
     local user_id = presence.user_id
-    if user_id == get_world(world_id).master then
-        remove_master(world_id)
+    if user_id == worlds.get_world(world_table, world_id).master then
+        worlds.remove_master(world_table, world_id)
     else
         -- Does not remove the reservation
-        remove_presence(world_id, presence)
+        worlds.remove_presence(world_table, world_id, presence)
     end
 end
 
@@ -135,7 +82,7 @@ end
 -- payload should be the world_id
 local function get_presences(_context, payload)
     -- nakama.logger_warn(string.format("get_presences: payload %s", payload))
-    local world = get_world(payload)
+    local world = worlds.get_world(world_table, payload)
     local ret = {}
     for k, v in ipairs(world.player_reservations) do
         ret[#ret+1] = {user_id = v, presence = world.player_presences[v] or false}
