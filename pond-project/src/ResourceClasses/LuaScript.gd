ef17839ce8e71b15517c67a673c91001820c050c
RogLeite <rodrigo.ldasilva@hotmail.com>
2022-12-22 10:12:11 -0300

    v0.3.0.80 Fixes LuaScript
    v0.3.0.80 Fixes bug in LuaScript loading
    
    # LuaScript.gd:
     - Fixes bug where LuaScript was not loaded correctly by leaving only data in the script

--- pond-project/src/ResourceClasses/LuaScript.gd
+++ pond-project/src/ResourceClasses/LuaScript.gd
@@ -1,13 +1,4 @@
 extends Resource
 class_name LuaScript
 
-export(String, MULTILINE) var lua_script : String = "swim(0)" setget set_lua_script
-
-func _init(p_lua_script : String):
-	lua_script = p_lua_script
-
-func set_lua_script(p_lua_script : String) -> void:
-	lua_script = p_lua_script
-
-func get_class() -> String:
-	return "LuaScript"
+export(String, MULTILINE) var lua_script : String = "swim(0)"
