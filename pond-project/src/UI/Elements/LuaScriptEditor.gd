6b4cc1d83c11a37f1af3f275435a06186f4e4140
RogLeite <rodrigo.ldasilva@hotmail.com>
2023-05-19 11:19:09 -0300

    v0.3.0.179 Colours comments and strings
    v0.3.0.179 Colours comments and strings
    
    # LuaScriptEditor.gd:
     - Colours string made from `'` and `"`
     - Colours line comments
     - Colours multiline comments up to 50 repetitions of `=`

--- pond-project/src/UI/Elements/LuaScriptEditor.gd
+++ pond-project/src/UI/Elements/LuaScriptEditor.gd
@@ -7,6 +7,8 @@ const FLOW_CONTROL_KEYWORDS = ["until", "while", "else", "elseif", "for", "goto"
 
 export var keyword_color : Color = Color(1, 0.439216, 0.521569, 1)
 export var flow_control_keyword_color : Color = Color(1, 0.54902, 0.8, 1)
+export var string_color : Color = Color(1, 0.92549, 0.631373)
+export var comment_color : Color = Color(0.462745, 0.47451, 0.509804)
 #export var symbol_color : Color = Color(1, 0.54902, 0.8, 1)
 
 func _init():
@@ -16,3 +18,15 @@ func _init():
 		add_keyword_color(word, flow_control_keyword_color)
 #	for word in KEYWORDS:
 #		add_keyword_color(word, keyword_color)
+
+	# Colours strings
+	add_color_region("\"", "\"", string_color)
+	add_color_region("\'", "\'", string_color)
+	
+	# Colours comments
+	for i in range(0,50):
+		var s : String = "=".repeat(i)
+		add_color_region("--[%s["%s, "]%s]"%s, comment_color)
+	add_color_region("--", "", comment_color, true)
+
+	
